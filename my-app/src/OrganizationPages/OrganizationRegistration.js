2df764d7f0dad4a309110015596f61706bc18f55
john <john.n.pham2000@gmail.com>
2021-09-25 19:59:30 -0400

    Added state hooks to registration form, Added correct form fields

--- my-app/src/OrganizationPages/OrganizationRegistration.js
+++ my-app/src/OrganizationPages/OrganizationRegistration.js
@@ -2,19 +2,80 @@ import React, { useState } from "react";
 import { Link, withRouter } from "react-router-dom";
 
 function OrganizationReg() {
-	function getTags() {
-		const tags = [];
-		for (var i = 0; i < 13; i++) {
-			tags.push(
-				<text className="text-xs rounded-lg bg-gray-100 border-gray-300 border hover:bg-gray-50 px-4 py-1 text-dark-blueGray">
-					{" "}
-					Tag - 1{" "}
-				</text>
-			);
-		}
 
-		return tags;
+	const [chosenTags, setTags] = useState([])
+	const [tagStyle, setTagStyle] = useState([false, false, false, false, false, false, false, false])
+	const [registerInfo, setRegister] = useState({
+		name: "",
+		password: "",
+		interests: [],
+		phone: "",
+		email: "",
+		school: "",
+	})
+
+	const handleChange = (e) => {
+		const {name, value} = e.target
+		setRegister(prevState => ({
+			...prevState,
+			[name]: value
+		}))
+		console.log(e.target.value)
 	}
+
+	// Toggles a tag on or off
+	const toggleTag = (tag, index) => {
+		let temp = [...(registerInfo.interests)]
+		let temp2 = [...tagStyle];
+
+		if (!temp.includes(tag))
+		{
+			temp.push(tag)
+		}	
+		else
+		{
+			let index = temp.indexOf(tag);
+			temp.splice(index, 1)	
+		}
+		
+		temp2[index] = !temp2[index];
+		setRegister({
+			...registerInfo,
+			interests: temp 
+		})
+		setTagStyle(temp2);
+	}
+
+	// Renders our list of tags
+	const getTags = () => {
+		const tags = [
+		"STEM",
+		"Computer Science",
+		"Social",
+		"LifeStyle",
+		"Business",
+		"Health",
+		"Food",
+		"Other"
+	];
+		let tagList = tags.map((tag, index) => {
+			let selectedOn = "text-xs rounded-lg bg-red-100 border-pink-500 border hover:bg-red-50 px-4 py-1 text-dark-blueGray"
+			let selectedOff = "text-xs rounded-lg bg-gray-100 border-gray-300 border hover:bg-red-50 px-4 py-1 text-dark-blueGray"
+
+			return (
+					<text onClick={() => {toggleTag(tags[index], index)}} className={tagStyle[index] ? selectedOn : selectedOff}>
+						{" "}
+						{tags[index]}{" "}
+					
+					</text>
+			)
+		})
+
+		return tagList;
+	}
+
+	const inputStyle = "shadow appearance-none border border-pink-500 rounded lg:w-80 w-auto py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+	
 	return (
 		<div className="grid gird-rows-2">
 			{/*lOGO*/}
@@ -101,46 +162,91 @@ function OrganizationReg() {
 					<form>
 						<div className="mb-8">
 							<label
-								className="block text-gray-700 text-sm font-bold mb-2"
-								for="text"
+								className="block text-gray-700  text-sm font-bold mb-2"
+								for="username"
 							>
-								What is the name of your Organization?
+								Username
 							</label>
 							<input
-								className="shadow appearance-none border rounded lg:w-80 w-auto py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+								className={inputStyle}
 								id="username"
-								type="text"
+								name="name"
+								onChange={(event) => handleChange(event)}
 								placeholder="Username"
+								type="text"
+								value={registerInfo.name}
 							/>
 						</div>
 						<div className="mb-8">
 							<label
 								className="block text-gray-700 text-sm font-bold mb-2"
 								for="password"
+							>
+								Password
+							</label>
+							<input
+								className={inputStyle}
+								id="password"
+								type="password"
+								name="password"
+								onChange={event => handleChange(event)}
+								placeholder="*****"
+								value={registerInfo.password}
+							/>
+						</div>
+
+						<div className="mb-8">
+							<label
+								className="block text-gray-700 text-sm font-bold mb-2"
+								for="phone"
+							>
+								Phone
+							</label>
+							<input
+								className={inputStyle}
+								id="phone"
+								type="tele"
+								placeholder="123-456-7890"
+								onChange={event => handleChange(event)}
+								value={registerInfo.phone}
+								name="phone"
+							/>
+						</div>
+						<div className="mb-8">
+							<label
+								className="block text-gray-700 text-sm font-bold mb-2"
+								for="email"
 							>
 								Email
 							</label>
 							<input
-								className="shadow appearance-none border border-pink-500 rounded lg:w-80 w-auto py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+								className={inputStyle}
 								id="email"
 								type="email"
-								placeholder="email@domain.com"
+								placeholder="something@gmail.com"
+								name="email"
+								onChange={event => handleChange(event)}
+								value={registerInfo.email}
 							/>
 						</div>
 						<div className="mb-8">
 							<label
 								className="block text-gray-700 text-sm font-bold mb-2"
-								for="password"
+								for="school"
 							>
-								Tell us about your organization!
+								School Name
 							</label>
-							<textarea
-								className="h-64 shadow appearance-none border border-pink-500 rounded lg:w-80 w-auto py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-								id="email"
-								type="email"
-								placeholder="Write a few sentences about what your organization does and do not forget to provide links to your social media sites and websites!"
+							<input
+								className={inputStyle}
+								id="school"
+								type="text"
+								placeholder="Something University"
+								name="school"
+								onChange={event => handleChange(event)}
+								value={registerInfo.school}
 							/>
 						</div>
+						
 						<div className="mb-8">
 							<label
 								className="block text-gray-700 text-sm font-bold mb-2"
@@ -150,56 +256,22 @@ function OrganizationReg() {
 							</label>
 							<div className="flex flex-auto flex-wrap gap-4 lg:w-96 w-auto  items-center justify-center">
 								{getTags()}
+								{/* {registerInfo.interests.map((tag, index) => {
+									return tag;
+								})} */}
 							</div>
 						</div>
 						<div className="flex items-center justify-center">
 							<button
 								className="bg-pink-500 hover:bg-pink-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+								onClick={() => console.log(registerInfo)}
 								type="button"
 							>
 								Register
 							</button>
 						</div>
+						
 					</form>
-				</div>
-				<div id="step-2" className="hidden">
-					<svg
-						width="147"
-						height="42"
-						viewBox="0 0 147 42"
-						fill="none"
-						xmlns="http://www.w3.org/2000/svg"
-					>
-						<path
-							d="M63.6407 28.14C62.614 28.14 61.694 27.9733 60.8807 27.64C60.0674 27.3067 59.414 26.8133 58.9207 26.16C58.4407 25.5067 58.1874 24.72 58.1607 23.8H61.8007C61.854 24.32 62.034 24.72 62.3407 25C62.6474 25.2667 63.0474 25.4 63.5407 25.4C64.0474 25.4 64.4474 25.2867 64.7407 25.06C65.034 24.82 65.1807 24.4933 65.1807 24.08C65.1807 23.7333 65.0607 23.4467 64.8207 23.22C64.594 22.9933 64.3074 22.8067 63.9607 22.66C63.6274 22.5133 63.1474 22.3467 62.5207 22.16C61.614 21.88 60.874 21.6 60.3007 21.32C59.7274 21.04 59.234 20.6267 58.8207 20.08C58.4074 19.5333 58.2007 18.82 58.2007 17.94C58.2007 16.6333 58.674 15.6133 59.6207 14.88C60.5674 14.1333 61.8007 13.76 63.3207 13.76C64.8674 13.76 66.114 14.1333 67.0607 14.88C68.0074 15.6133 68.514 16.64 68.5807 17.96H64.8807C64.854 17.5067 64.6874 17.1533 64.3807 16.9C64.074 16.6333 63.6807 16.5 63.2007 16.5C62.7874 16.5 62.454 16.6133 62.2007 16.84C61.9474 17.0533 61.8207 17.3667 61.8207 17.78C61.8207 18.2333 62.034 18.5867 62.4607 18.84C62.8874 19.0933 63.554 19.3667 64.4607 19.66C65.3674 19.9667 66.1007 20.26 66.6607 20.54C67.234 20.82 67.7274 21.2267 68.1407 21.76C68.554 22.2933 68.7607 22.98 68.7607 23.82C68.7607 24.62 68.554 25.3467 68.1407 26C67.7407 26.6533 67.154 27.1733 66.3807 27.56C65.6074 27.9467 64.694 28.14 63.6407 28.14ZM76.6854 25.1V28H74.9454C73.7054 28 72.7387 27.7 72.0454 27.1C71.3521 26.4867 71.0054 25.4933 71.0054 24.12V19.68H69.6454V16.84H71.0054V14.12H74.4254V16.84H76.6654V19.68H74.4254V24.16C74.4254 24.4933 74.5054 24.7333 74.6654 24.88C74.8254 25.0267 75.0921 25.1 75.4654 25.1H76.6854ZM89.2104 16.84V28H85.7904V26.48C85.4437 26.9733 84.9704 27.3733 84.3704 27.68C83.7837 27.9733 83.1304 28.12 82.4104 28.12C81.5571 28.12 80.8037 27.9333 80.1504 27.56C79.4971 27.1733 78.9904 26.62 78.6304 25.9C78.2704 25.18 78.0904 24.3333 78.0904 23.36V16.84H81.4904V22.9C81.4904 23.6467 81.6837 24.2267 82.0704 24.64C82.4571 25.0533 82.9771 25.26 83.6304 25.26C84.2971 25.26 84.8237 25.0533 85.2104 24.64C85.5971 24.2267 85.7904 23.6467 85.7904 22.9V16.84H89.2104ZM90.587 22.4C90.587 21.2533 90.8003 20.2467 91.227 19.38C91.667 18.5133 92.2603 17.8467 93.007 17.38C93.7536 16.9133 94.587 16.68 95.507 16.68C96.2403 16.68 96.907 16.8333 97.507 17.14C98.1203 17.4467 98.6003 17.86 98.947 18.38V13.2H102.367V28H98.947V26.4C98.627 26.9333 98.167 27.36 97.567 27.68C96.9803 28 96.2936 28.16 95.507 28.16C94.587 28.16 93.7536 27.9267 93.007 27.46C92.2603 26.98 91.667 26.3067 91.227 25.44C90.8003 24.56 90.587 23.5467 90.587 22.4ZM98.947 22.42C98.947 21.5667 98.707 20.8933 98.227 20.4C97.7603 19.9067 97.187 19.66 96.507 19.66C95.827 19.66 95.247 19.9067 94.767 20.4C94.3003 20.88 94.067 21.5467 94.067 22.4C94.067 23.2533 94.3003 23.9333 94.767 24.44C95.247 24.9333 95.827 25.18 96.507 25.18C97.187 25.18 97.7603 24.9333 98.227 24.44C98.707 23.9467 98.947 23.2733 98.947 22.42ZM114.961 22.24C114.961 22.56 114.941 22.8933 114.901 23.24H107.161C107.215 23.9333 107.435 24.4667 107.821 24.84C108.221 25.2 108.708 25.38 109.281 25.38C110.135 25.38 110.728 25.02 111.061 24.3H114.701C114.515 25.0333 114.175 25.6933 113.681 26.28C113.201 26.8667 112.595 27.3267 111.861 27.66C111.128 27.9933 110.308 28.16 109.401 28.16C108.308 28.16 107.335 27.9267 106.481 27.46C105.628 26.9933 104.961 26.3267 104.481 25.46C104.001 24.5933 103.761 23.58 103.761 22.42C103.761 21.26 103.995 20.2467 104.461 19.38C104.941 18.5133 105.608 17.8467 106.461 17.38C107.315 16.9133 108.295 16.68 109.401 16.68C110.481 16.68 111.441 16.9067 112.281 17.36C113.121 17.8133 113.775 18.46 114.241 19.3C114.721 20.14 114.961 21.12 114.961 22.24ZM111.461 21.34C111.461 20.7533 111.261 20.2867 110.861 19.94C110.461 19.5933 109.961 19.42 109.361 19.42C108.788 19.42 108.301 19.5867 107.901 19.92C107.515 20.2533 107.275 20.7267 107.181 21.34H111.461ZM123.165 16.72C124.472 16.72 125.512 17.1467 126.285 18C127.072 18.84 127.465 20 127.465 21.48V28H124.065V21.94C124.065 21.1933 123.872 20.6133 123.485 20.2C123.099 19.7867 122.579 19.58 121.925 19.58C121.272 19.58 120.752 19.7867 120.365 20.2C119.979 20.6133 119.785 21.1933 119.785 21.94V28H116.365V16.84H119.785V18.32C120.132 17.8267 120.599 17.44 121.185 17.16C121.772 16.8667 122.432 16.72 123.165 16.72ZM135.662 25.1V28H133.922C132.682 28 131.715 27.7 131.022 27.1C130.329 26.4867 129.982 25.4933 129.982 24.12V19.68H128.622V16.84H129.982V14.12H133.402V16.84H135.642V19.68H133.402V24.16C133.402 24.4933 133.482 24.7333 133.642 24.88C133.802 25.0267 134.069 25.1 134.442 25.1H135.662ZM141.707 28.16C140.734 28.16 139.867 27.9933 139.107 27.66C138.347 27.3267 137.747 26.8733 137.307 26.3C136.867 25.7133 136.62 25.06 136.567 24.34H139.947C139.987 24.7267 140.167 25.04 140.487 25.28C140.807 25.52 141.2 25.64 141.667 25.64C142.094 25.64 142.42 25.56 142.647 25.4C142.887 25.2267 143.007 25.0067 143.007 24.74C143.007 24.42 142.84 24.1867 142.507 24.04C142.174 23.88 141.634 23.7067 140.887 23.52C140.087 23.3333 139.42 23.14 138.887 22.94C138.354 22.7267 137.894 22.4 137.507 21.96C137.12 21.5067 136.927 20.9 136.927 20.14C136.927 19.5 137.1 18.92 137.447 18.4C137.807 17.8667 138.327 17.4467 139.007 17.14C139.7 16.8333 140.52 16.68 141.467 16.68C142.867 16.68 143.967 17.0267 144.767 17.72C145.58 18.4133 146.047 19.3333 146.167 20.48H143.007C142.954 20.0933 142.78 19.7867 142.487 19.56C142.207 19.3333 141.834 19.22 141.367 19.22C140.967 19.22 140.66 19.3 140.447 19.46C140.234 19.6067 140.127 19.8133 140.127 20.08C140.127 20.4 140.294 20.64 140.627 20.8C140.974 20.96 141.507 21.12 142.227 21.28C143.054 21.4933 143.727 21.7067 144.247 21.92C144.767 22.12 145.22 22.4533 145.607 22.92C146.007 23.3733 146.214 23.9867 146.227 24.76C146.227 25.4133 146.04 26 145.667 26.52C145.307 27.0267 144.78 27.4267 144.087 27.72C143.407 28.0133 142.614 28.16 141.707 28.16Z"
-							fill="#A21CAF"
-						/>
-						<path
-							d="M39 29.5315V1.96855C39 0.877637 38.0686 0 36.9105 0H8.35715C3.74331 0 0 3.52746 0 7.87527V34.1247C0 38.4725 3.74331 42 8.35715 42H36.9105C38.0686 42 39 41.1224 39 40.0315V38.7185C39 38.1033 38.6953 37.5458 38.225 37.1847C37.8598 35.9215 37.8598 32.3204 38.225 31.0571C38.6953 30.7042 39 30.1467 39 29.5315ZM11.143 10.992C11.143 10.7215 11.3779 10.5 11.6651 10.5H30.1204C30.4076 10.5 30.643 10.7215 30.643 10.992V12.6327C30.643 12.9033 30.4076 13.1247 30.1204 13.1247H11.6651C11.3779 13.1247 11.143 12.9033 11.143 12.6327V10.992ZM11.143 16.242C11.143 15.9715 11.3779 15.75 11.6651 15.75H30.1204C30.4076 15.75 30.643 15.9715 30.643 16.242V17.8827C30.643 18.1533 30.4076 18.3753 30.1204 18.3753H11.6651C11.3779 18.3753 11.143 18.1533 11.143 17.8827V16.242ZM33.2023 36.75H8.35715C6.81628 36.75 5.57145 35.5767 5.57145 34.1247C5.57145 32.6815 6.825 31.5 8.35715 31.5H33.2023C33.0367 32.9029 33.0367 35.3471 33.2023 36.75Z"
-							fill="#701A75"
-						/>
-					</svg>
-					<text className="text-4xl font-black text-dark-blueGray">
-						{" "}
-						Register Now{" "}
-					</text>
-					<div className="flex items-center content-center justify-center"></div>
-					<text className="text-2xl"> Please verify your phone number. </text>
-					{/* From TailwindCSS Samples */}
-					<div className=" md:flex md:items-center mb-8">
-						<div className="md:w-2/3">
-							<input
-								className="bg-gray-200 appearance-none border-2 border-gray-200 rounded mt-4 py-2 px-4 text-gray-700 w-64 leading-tight focus:outline-none focus:bg-white focus:border-fuchsia-600"
-								type="text"
-								deafultValue=""
-								placeholder="Verification"
-							/>
-						</div>
-						<button className="ml-4 mt-4 rounded h-full py-2 px-4 text-white font-bold  bg-fuchsia-600 hover:bg-fuchsia-500  focus:outline-none focus:ring-2 focus:bg-fuchsia-500  focus:ring-opacity-50">
-							{" "}
-							Register{" "}
-						</button>
-					</div>
 				</div>
 			</div>
 		</div>
