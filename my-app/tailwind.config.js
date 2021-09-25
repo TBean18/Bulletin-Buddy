816c37d7199d48ea074277aa1528ca2b6aed99d5
Victoria Williamson <vicwill01@knights.ucf.edu>
2021-09-25 10:30:01 -0400

    UI for landing page added and responsive

--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
     purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
     darkMode: false, // or 'media' or 'class'
     theme: {
-      extend: {},
+      extend: {
+          colors: {
+              'dark-blueGray': '#1E293B',
+              'light-blueGray': '#F1F5F9',
+              'fuchsia-500': '#D946EF',
+              'fuchsia-600': '#C026D3',
+          }
+      },
     },
     variants: {
       extend: {},
