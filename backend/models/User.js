7afd8c4242f42fe5fff7d557cba0491bf3842a46
Taylor Bean <JacobBean18@gmail.com>
2021-09-25 16:13:04 -0400

    Initial Working API User(Login, register) Hub(View Boards) installed deps

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt");
 
 const UserSchema = new Schema({
   name: {
@@ -15,17 +16,54 @@ const UserSchema = new Schema({
   phone_number: {
     type: String,
     required: true,
+    unique: true,
+    dropDups: true,
   },
   email: {
     type: String,
     required: true,
+    unique: true,
+    dropDups: true,
   },
   school: {
     type: String,
     required: true,
     // What about schools that dont exist.
   },
+  password_hash: {
+    type: String,
+    required: true,
+  },
 });
+
+// On save method, that hashes the password if it detects that password has been changed
+UserSchema.pre("save", function (next) {
+  let user = this;
+
+  if (!user.isModified("password_hash")) {
+    return next();
+  }
+
+  // Generate the Salt
+  bcrypt.genSalt(10, function (err, salt) {
+    if (err) return next(err);
+
+    //hash the password
+    bcrypt.hash(user.password_hash, salt, function (err, hash) {
+      if (err) return next(err);
+
+      user.password_hash = hash;
+      next();
+    });
+  });
+});
+
+UserSchema.methods.comparePassword = function (inputPassword, cb) {
+  bcrypt.compare(inputPassword, this.password_hash, (err, isMatch) => {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
 
 const model = mongoose.model("user", UserSchema);
 module.exports = { model, UserSchema };
