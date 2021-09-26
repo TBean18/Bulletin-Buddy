d8fdc3a240d27e618157046a0c2347ddb0d587dd
Victoria Williamson <vicwill01@knights.ucf.edu>
2021-09-26 00:19:57 -0400

    added registration page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
+import PostCard from './PostTools/PostCard';
 import HubPage from "./HubPages/Hub.js";
 import Landing from "./Landing.js";
 import StudentReg from "./StudentPages/StudentRegistration.js";
@@ -10,7 +11,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 function App() {
 	return (
 		<div>
-			
+			<PostCard/>
 			<Router>
 				<Switch>
 					<Route path="/" exact component={() => <Landing />} />
