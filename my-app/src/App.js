916a5cada40404a1fd99e19e40f98861bb15dea0
Taylor Bean <JacobBean18@gmail.com>
2021-09-26 00:48:08 -0400

    Fixed Merge Conflict

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import PostCard from './PostTools/PostCard';
+import PostCard from "./PostTools/PostCard";
 import HubPage from "./HubPages/Hub.js";
 import Landing from "./Landing.js";
 import StudentReg from "./StudentPages/StudentRegistration.js";
@@ -11,29 +11,6 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { GlobalProvider } from "./context/GlobalState";
 
 function App() {
-<<<<<<< HEAD
-	return (
-		<div>
-			<PostCard/>
-			<Router>
-				<Switch>
-					<Route path="/" exact component={() => <Landing />} />
-					<Route path="/student/reg" exact component={() => <StudentReg />} />
-					<Route
-						path="/organization/reg"
-						exact
-						component={() => <OrganizationReg />}
-					/>
-					<Route 
-						path="/hub" 
-						exact 
-						component={() => <HubPage/>}
-					/>
-				</Switch>
-			</Router> 
-		</div>
-	);
-=======
   return (
     <GlobalProvider>
       <div>
@@ -57,7 +34,6 @@ function App() {
       </div>
     </GlobalProvider>
   );
->>>>>>> 63a45650141bee57b773d404e677d7d36febb105
 }
 
 export default App;
