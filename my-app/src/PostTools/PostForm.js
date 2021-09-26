fc519ee1d88af5643052abd5990f839a66bf0c7e
john <john.n.pham2000@gmail.com>
2021-09-25 23:35:16 -0400

    Initial commit for form

---
+++ my-app/src/PostTools/PostForm.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const PostForm = () => {
+	return (
+		<div className="grid grid-cols-3 gap-4">
+			<div></div>
+			<div></div>
+			<div></div>
+		</div>
+	)
+}
+
+export default PostForm
---
+++ my-app/src/PostTools/PostFormContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PostFormContainer = () => {
+	return (
+		<div className="grid grid-cols-3 gap-4">
+			
+		</div>
+	)
+}
+
+export default PostFormContainer
