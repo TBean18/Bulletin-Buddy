06d7eb42d7572a7e4777cc5654961cdb78d40f7d
Taylor Bean <JacobBean18@gmail.com>
2021-09-26 05:28:42 -0400

    redirect on post creation

--- my-app/src/PostTools/PostCard.js
+++ my-app/src/PostTools/PostCard.js
@@ -1,4 +1,5 @@
 import React, { useContext, useState } from "react";
+import { useHistory } from "react-router";
 import { GlobalContext } from "../context/GlobalState";
 
 const PostCard = () => {
@@ -9,6 +10,7 @@ const PostCard = () => {
   const [location, setLocation] = useState("");
   const [image_address, setImage_Address] = useState("");
   const [tags, setTags] = useState("");
+  let history = useHistory();
 
   // Tags
   const [chosenTags, setChosenTags] = useState([]);
@@ -55,6 +57,7 @@ const PostCard = () => {
       console.log(`OH NO! and error has been detected: \n ${body.error}`);
     } else {
       console.log(`Hooray!! : ${body} `);
+      history.push("/hub");
     }
   }
 
@@ -112,33 +115,54 @@ const PostCard = () => {
       <div className="grid grid-row-2 gap-4 m-10">
         {/* <div className="bg-white rounded-lg w-full  h-96 p-6"></div>
         <div className="bg-white rounded-lg w-full  h-96 p-6"></div> */}
-       <div className="bg-white rounded-lg w-5/6 overflow-y-auto max-h-5/6 p-6">
-        <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                <div className="sm:flex sm:items-start">
-                  <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                    <h3  className="text-4xl text-center mt-8 leading-6 font-black text-gray-900">
-                      Title
-                      </h3>
-                   
-                    <div className="mt-2">
-                      <p className="text-sm text-gray-500">
-                          <div id="flex flex-cols items-center justify-center">
-                            <button id="interest" className="rounded-xl h-5 p-4">
-                               
-                            </button>
-                          <div> <text className="font-bold text-lg"> Date : </text>September 25, 2021</div>
-                          <div> <text className="font-bold text-lg">Location : </text> Orlando,FL</div>
-                          <br/>
-                      <img className="h-auto w-full" src="https://previews.customer.envatousercontent.com/files/308521832/Final%20Presentation%20single.jpg"/>
-                      <br/>
-                      <div> <text className="font-bold text-lg"> Description : </text> Hi tell some information about the organizatino here. Clubs are fun etc etc.</div>
-                          </div>
-                      
-                      </p>
+        <div className="bg-white rounded-lg w-5/6 overflow-y-auto max-h-5/6 p-6">
+          <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+            <div className="sm:flex sm:items-start">
+              <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                <h3 className="text-4xl text-center mt-8 leading-6 font-black text-gray-900">
+                  Title
+                </h3>
+
+                <div className="mt-2">
+                  <p className="text-sm text-gray-500">
+                    <div id="flex flex-cols items-center justify-center">
+                      <button
+                        id="interest"
+                        className="rounded-xl h-5 p-4"
+                      ></button>
+                      <div>
+                        {" "}
+                        <text className="font-bold text-lg"> Date : </text>
+                        September 25, 2021
+                      </div>
+                      <div>
+                        {" "}
+                        <text className="font-bold text-lg">
+                          Location :{" "}
+                        </text>{" "}
+                        Orlando,FL
+                      </div>
+                      <br />
+                      <img
+                        className="h-auto w-full"
+                        src="https://previews.customer.envatousercontent.com/files/308521832/Final%20Presentation%20single.jpg"
+                      />
+                      <br />
+                      <div>
+                        {" "}
+                        <text className="font-bold text-lg">
+                          {" "}
+                          Description :{" "}
+                        </text>{" "}
+                        Hi tell some information about the organizatino here.
+                        Clubs are fun etc etc.
+                      </div>
                     </div>
-                  </div>
+                  </p>
                 </div>
               </div>
+            </div>
+          </div>
         </div>
       </div>
       <div className="bg-white rounded-lg w-5/6  h-5/6 p-6">
@@ -247,7 +271,9 @@ const PostCard = () => {
               />
             </div>
 
-            <div className="mb-4 gap-2 flex flex-wrap flex-row px-32 items-center justify-center">{getTags()}</div>
+            <div className="mb-4 gap-2 flex flex-wrap flex-row px-32 items-center justify-center">
+              {getTags()}
+            </div>
           </form>
           <button
             onClick={() => makePost()}
