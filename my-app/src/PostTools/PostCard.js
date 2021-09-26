070ead1b01c42484c8455828f6b4941d79b08458
Victoria Williamson <vicwill01@knights.ucf.edu>
2021-09-26 02:55:11 -0400

    cleaning up more UI

--- my-app/src/PostTools/PostCard.js
+++ my-app/src/PostTools/PostCard.js
@@ -1,19 +1,46 @@
 import React, { useState } from "react";
-
+import { Dialog, Transition } from '@headlessui/react'
 const PostCard = () => {
 	
 
   return (
-    <div className="grid grid-cols-2 items-center justify-center h-screen w-screen m-10">
-      <div className="grid grid-row-2 gap-4 m-10">
-        <div className="bg-white rounded-lg w-full  h-96 p-6"></div>
-        <div className="bg-white rounded-lg w-full  h-96 p-6"></div>
+    <div className="lg:grid lg:grid-cols-2 flex flex-row items-center justify-center h-5/6 w-screen mx-10">
+      <div className=" gap-4 m-10">
+        <div className="bg-white rounded-lg w-5/6 overflow-y-auto max-h-5/6 p-6">
+        <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                <div className="sm:flex sm:items-start">
+                  <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                    <h3  className="text-4xl text-center mt-8 leading-6 font-black text-gray-900">
+                      Title
+                      </h3>
+                   
+                    <div className="mt-2">
+                      <p className="text-sm text-gray-500">
+                          <div id="flex flex-cols items-center justify-center">
+                            <button id="interest" className="rounded-xl h-5 p-4">
+                               
+                            </button>
+                          <div> <text className="font-bold text-lg"> Date : </text>September 25, 2021</div>
+                          <div> <text className="font-bold text-lg">Location : </text> Orlando,FL</div>
+                          <br/>
+                      <img className="h-auto w-full" src="https://previews.customer.envatousercontent.com/files/308521832/Final%20Presentation%20single.jpg"/>
+                      <br/>
+                      <div> <text className="font-bold text-lg"> Description : </text> Hi tell some information about the organizatino here. Clubs are fun etc etc.</div>
+                          </div>
+                      
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+        </div>
+       
       </div>
-      <div className="bg-white rounded-lg w-5/6  h-5/6 p-6">
+      <div className="bg-white rounded-lg w-5/6  my-8 max-h-full p-6">
         <div className="flex flex-col items-center justify-center">
           <text className="text-4xl my-4  font-black text-dark-blueGray">
             {" "}
-            Register Now{" "}
+            Create a Post{" "}
           </text>
           <form>
             <div className="mb-8">
